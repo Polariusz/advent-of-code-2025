5cd3e5ccbbb43772262414ea275f07c16bcd68b1
Polariusz <truepolariusz@gmail.com>
2025-12-01 18:47:50 +0100

    love me some nested if statements

--- day1/main.c
+++ day1/main.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void addDirection(char direction, int distance, int *currentRotation, int *cntZeroesPartOne, int *cntZeroesPartTwo)
+void rotate(char direction, int distance, int *currentRotation, int *cntZeroesPartOne, int *cntZeroesPartTwo)
 {
+	printf("Curr: %d next: %c-%d\n", *currentRotation, direction, distance);
+	int addedPartTwo = 0;
+	while(distance > 100) {
+		distance -= 100;
+		++(*cntZeroesPartTwo);
+		++addedPartTwo;
+	}
+
 	if(direction == 'L') {
-		*currentRotation -= distance;
-		while(*currentRotation < 0) {
+		if(*currentRotation - distance < 0) {
+			if(*currentRotation != 0) {
+				++(*cntZeroesPartTwo);
+				++addedPartTwo;
+			}
 			*currentRotation += 100;
-			++(*cntZeroesPartTwo);
 		}
+		*currentRotation -= distance;
 	} else if(direction == 'R') {
-		*currentRotation += distance;
-		while(*currentRotation > 99) {
+		if(*currentRotation + distance > 99) {
+			if(*currentRotation != 0 && *currentRotation + distance != 100) {
+				++(*cntZeroesPartTwo);
+				++addedPartTwo;
+			}
 			*currentRotation -= 100;
-			++(*cntZeroesPartTwo);
 		}
+		*currentRotation += distance;
 	} else {
 		printf("Unknown direction detected, it's '%c'\n", direction);
 	}
 	if(*currentRotation == 0) {
 		++(*cntZeroesPartOne);
 	}
+
+	printf("\taddedPartTwo: %d\n", addedPartTwo);
 }
 
 int main(int carg, char **varg)
@@ -42,11 +58,13 @@ int main(int carg, char **varg)
 	char direction = '\0';
 	int distance = 0;
 	while(fscanf(fp, "%c%d\n", &direction, &distance) != EOF) {
-		addDirection(direction, distance, &currentRotation, &cntZeroesPartOne, &cntZeroesPartTwo);
+		rotate(direction, distance, &currentRotation, &cntZeroesPartOne, &cntZeroesPartTwo);
 	}
 	fclose(fp);
-	printf("Password for part 1: %d\n", cntZeroesPartOne);
-	printf("Password for part 2: %d\n", cntZeroesPartTwo);
+	printf("Password for ONLY part 1: %d\n", cntZeroesPartOne);
+	printf("Password for ONLY part 2: %d\n", cntZeroesPartTwo);
+	printf("Password for part 2     : %d\n", cntZeroesPartOne + cntZeroesPartTwo);
+
 	printf("Just Monika\n");
 	return 0;
 }
